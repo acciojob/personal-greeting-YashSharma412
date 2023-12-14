0a1eb77c7e76d0b7e142512300ae92674e814fc7
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-12-14 06:27:31 +0000

    First initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name, setName] = useState("")
+  function handleName(e){
+    let myname = e.target.value;
+    if(myname === ""){
+      setName("")
+    } else {
+      setName("Hello " + myname);
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" onChange={handleName}></input>
+        <p>{name}</p>
     </div>
   )
 }
